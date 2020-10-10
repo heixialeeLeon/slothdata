e3569f7f895a7ac581906d11598a6cf1e35ad128
leon <heixialee@gmail.com>
2020-10-10 00:10:51 -0700

    add the download img function

---
+++ download_offtake_img.py
@@ -0,0 +1,13 @@
+import os
+import os.path as osp
+import glob
+from utils.download_img import download_img_from_csv_folder, download_img_from_csv_file
+
+target_folder = "imgs/offtake_img"
+
+if not osp.exists(target_folder):
+    os.makedirs(target_folder)
+
+for csv_file in glob.glob("offtake_csv/*.csv"):
+    print("process : {}".format(csv_file))
+    download_img_from_csv_file(csv_file, target_folder)
---
+++ utils/download_img.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import urllib
+import urllib.request
+import os
+import glob
+
+def download_img_from_csv_folder(csv_folder, img_path):
+    file_pattern = os.path.join(csv_folder,"*.csv")
+    for csv_file in glob.glob(file_pattern):
+        download_img_from_csv_file(csv_file, img_path)
+
+def download_img_from_csv_file(csv_file, img_path):
+    df = pd.read_csv(csv_file)
+    urls = df["ImageUrl"].unique()
+    for url in urls:
+        file_name = url.split('/')[-1] + ".jpg"
+        file_path = os.path.join(img_path, file_name)
+        download_from_url(url, file_path)
+    print("finish download csv: {}".format(csv_file))
+
+def download_from_url(url, file_name):
+    try:
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        get_img = response.read()
+        with open(file_name, 'wb') as fp:
+            fp.write(get_img)
+    except:
+        print('download failed')
